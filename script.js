9ecd23b2c34ab6b6c4c03183f11eca81c12edbf3
ShivamP-97 <shivampathak19972@gmail.com>
2025-11-23 23:08:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function isFibonacci(num) {
-		// Write your code here
+    if (typeof num !== "number" || num < 0 || !Number.isInteger(num)) {
+        return false;
+    }
+
+    if (num === 0 || num === 1) return true;
+
+    let a = 0, b = 1;
+    while (b < num) {
+        const next = a + b;
+        a = b;
+        b = next;
+    }
+
+    return b === num;
 }
-const input = parseInt(prompt("Enter a number"));
+
+const input = parseInt(prompt("Enter a non-negative integer:"));
 alert(isFibonacci(input));
